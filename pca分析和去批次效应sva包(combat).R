9930caa0762175ac81dd3bfca2b7d2257d614ba2
zerack <75766273+zerack5@users.noreply.github.com>
2022-08-25 16:50:28 +0800

    Add files via upload

--- pca分析和去批次效应sva包(combat).R
+++ pca分析和去批次效应sva包(combat).R
@@ -1,45 +1,38 @@
+#####################
+
+## 4. DESeq2包消除批次效应
+# design 设计矩阵中加入引起批次效应的因素(SR or PE)
+
+cbmdata <-as.matrix(cbdata)
+library(DESeq2)
+dds <- DESeqDataSetFromMatrix(countData = cbmdata,
+                              colData = colData,
+                              design = ~ condition+ type)
+dds <- DESeq(dds)
+resultsNames(dds)
+res <- results(dds, name="condition_untreated_vs_treated")
+
+summary(res)
+head(res)
+resOdered <- res[order(res$padj),]
+deg <- as.data.frame(resOdered)
+#deg <- na.omit(deg)
+dim(deg)
+#write.csv(deg,file= "diff_deseq2.csv")
+
+# 看看批次效应导致的差异表达基因,
+# 这里是测序方法SR(single-end)和PE (paired-end)
+batch_res <-  results(dds, name="type_SR_vs_PE")
+summary(batch_res)
+head(batch_res)
+batchResOdered <- res[order(batch_res$padj),]
+batchDeg <- as.data.frame(batchResOdered)
+batchDeg <- na.omit(batchDeg)
+dim(batchDeg)
+head(batchDeg)
 
 
-setwd("C:\\Users\\a\\Desktop\\Rrun1")
-######表达矩阵批量读入与合并######
-#先读入两个tsv检查gene_id和gene_name在不同样本中是否仍是一致的:
-test1 = data.table::fread("C:\\Users\\a\\Desktop\\Rrun1\\AS_Bulk\\mRNA\\MA07_NK\\abundance.tsv")
-test2 = data.table::fread("C:\\Users\\a\\Desktop\\Rrun1\\AS_Bulk\\mRNA\\MA14_CD4\\abundance.tsv")
-identical(test1$gene_id,test2$gene_id)#返回逻辑值TRUE，确认一致
-identical(test1$gene_name,test2$gene_name)#返回逻辑值TRUE，确认一致
-
-#批量读取所有tsv格式后缀的文件名：
-exp_all <- dir("C:\\Users\\a\\Desktop\\Rrun1\\AS_Bulk\\mRNA\\",
-               pattern = "*.abundance.tsv$",#*表示任意前缀，$表示固定后缀
-               recursive = T)
-head(exp_all)
-
-##注意，data.table::fread方式读入文件并不能指定行名，所以才使用这种方法
-x1 <- data.table::fread("AS_Bulk\\mRNA\\MA07_NK\\abundance.tsv",
-                        select = c("target_id"))
-head(x1)
-
-
-
-
-###这一行得改成用tximport的
-5
-
-#创建自定义函数,用于批量读入所有tcv文件的unstranded列（counts）：
-exp_dt <- function(x){
-  result <- data.table::fread(file.path("C:\\Users\\a\\Desktop\\Rrun1\\AS_Bulk\\mRNA\\",x),
-                              select = c("unstranded"))
-  return(result)
-}
-exp <- lapply(exp_all,exp_dt)#读取所有.tsv的unstranded列（将exp_all转换为list，并对list中的每一个元素都应用函数exp_dt）
-exp <- do.call(cbind,exp)#将exp按列合并，并将list转化为data.table
-exp[1:6,1:6]
-
-
-#添加行名(gene_id),所有表达矩阵的gene_id是相同的：
-exp <- as.data.frame(exp)#data.table格式不能自定行名，因此我们先转换为数据框
-rownames(exp) <- x1$gene_id
-exp[1:8,1:4]
+###################################################
 
 
 
@@ -47,7 +40,56 @@ exp[1:8,1:4]
 
 
 
- install.packages("tximport")
-aa
- BiocManager::install("DESeq2")
- ?repositories
+
+
+
+
+
+#BiocManager::install("sva")
+#BiocManager::install("bladderbatch")
+
+#建立批次效应的模型，data$type表示的是数据中除了有不同的批次，还有生物学上的差异。
+#在校正的时候要保留生物学上的差异，不能矫正过枉。
+dataas <- read.csv('expas.csv', sep = ',',header = T,row.names = 1)
+datac <- read.csv('expcontrol.csv', sep = ',',header = T,row.names = 1)
+cbdata <- cbind(dataas, datac)
+
+
+fzdata <- read.csv('注释信息.csv', sep = ',',header = T,row.names = 1)
+
+model <- model.matrix(~as.factor(fzdata$type))
+
+library("devtools")
+#install_github("kassambara/factoextra")
+library("FactoMineR")
+library("factoextra")
+af1.pca <- PCA(t(cbdata),graph = FALSE)
+combat.pca <- PCA(t(cbdata),graph = FALSE)
+fviz_pca_ind(combat.pca,
+             geom= "point",
+             col.ind = fzdata$label,
+             addEllipses = TRUE,
+             legend.title="Group"  )
+
+##消除批次效应
+library(sva)
+
+combat_Expr <- ComBat(dat = cbdata,batch = fzdata$batch,mod = model)
+
+##combat_Expr就是校正后的数据
+#BiocManager::install("FactoMineR")
+ 
+#BiocManager::install("factoextra")
+#install.packages("devtools")
+
+library("devtools")
+#install_github("kassambara/factoextra")
+library("FactoMineR")
+library("factoextra")
+af1.pca <- PCA(t(combat_Expr),graph = FALSE)
+combat.pca <- PCA(t(combat_Expr),graph = FALSE)
+fviz_pca_ind(combat.pca,
+             geom= "point",
+             col.ind = fzdata$label,
+             addEllipses = TRUE,
+             legend.title="Group"  )
